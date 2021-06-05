d4292274bcb83ef8ac80f146bf247e3f07a4a53d
Saul <saul@nixnux.org>
2021-06-04 18:38:22 -0700

    70-climbing-stairs initial commit

---
+++ 70-climbing-stairs/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+// You are climbing a staircase. It takes n steps to reach the top.
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+// Something something factoring and shit
+
+
+int climbStairs(int n) {
+    int output = 0;
+    // Any can be broken into 1's
+    
+
+    return 0;
+}
+
+int main() {
+    int test_case = 3;
+    // Expected output: 3
+    // 1. 1 step + 1 step + 1 step
+    // 2. 1 step + 2 steps
+    // 3. 2 steps + 1 step
+    // Actual output:
+
+    int result = climbStairs(test_case);
+    
+    std::cout << result << std::endl;
+
+    return 0;
+}
