32ea1fe8059f2a67b7435c3dd7e676adc9fd96b8
Saul <saul@nixnux.org>
2021-06-06 14:21:28 -0700

    4-median fixed

--- 4-median-of-two-sorted-arrays/main.cpp
+++ 4-median-of-two-sorted-arrays/main.cpp
@@ -1,6 +1,6 @@
-#include <cstdlib>
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 // Done
 
@@ -9,7 +9,7 @@ double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2) 
     std::vector<int> vec_b = nums2;
 
     int total_length = nums1.size() + nums2.size();
-    int half_length = total_length / 2; // 4
+    int half_length = total_length / 2;
 
     // Make sure vec_a is the smaller array
     if(vec_a.size() > vec_b.size()) {
@@ -18,19 +18,47 @@ double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2) 
         vec_b = temp_vec_c;
     }
 
-    int left = 0; // 2
-    int right = vec_a.size() - 1; // 3
-
+    int left = 0;
+    int right = vec_a.size() - 1;
     while (true) {
         // Splitting both arrays into two parts; The left two parts, if merged make up the left half of a combined sorted array 
-        int a_part_1 = (left + right) / 2;
-        int b_part_1 = half_length - a_part_1 - 2; 
+        int a_part_1 = floor(left + (right - left) / 2.0);
+        float b_part_1 = half_length - a_part_1 - 2;
 
-        int a_left = vec_a.at(a_part_1);
-        int a_right = vec_a.at(a_part_1 + 1);
+        int a_left;
+        int a_right;
+        int b_left;
+        int b_right;
 
-        int b_left = vec_b.at(b_part_1); 
-        int b_right = vec_b.at(b_part_1 + 1);
+        if (a_part_1 >= 0) {
+            a_left = vec_a.at(a_part_1);
+        } else {
+            a_left = -10000;
+        }
+
+        if (a_part_1 + 1 < vec_a.size()) {
+            a_right = vec_a.at(a_part_1 + 1);
+        } else {
+            a_right = 10000;
+        }
+
+
+        if (b_part_1 >= 0) {
+            b_left = vec_b.at(b_part_1);
+        } else {
+            b_left = -10000;
+        }
+
+        if (b_part_1 + 1 < vec_b.size()) {
+            b_right = vec_b.at(b_part_1 + 1);
+        } else {
+            b_right = 10000;
+        }
+
+        std::cout << a_left << std::endl;
+        std::cout << a_right << std::endl;
+        std::cout << b_left << std::endl;
+        std::cout << b_right << std::endl;
 
         // Check if the split is correct
         if (a_left <= b_right && b_left <= a_right) {
@@ -72,9 +100,9 @@ double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2) 
 
 int main() {
     // Test case
-    std::vector<int> input_a = {1, 2, 3, 5, 6};
+    std::vector<int> input_a = {1, 3};
     
-    std::vector<int> input_b = {1, 2, 2, 3};
+    std::vector<int> input_b = {2};
     // 1, 1, 2, 2, 2, 3, 3, 5, 6
 
     std::cout << findMedianSortedArrays(input_a, input_b) << std::endl;
