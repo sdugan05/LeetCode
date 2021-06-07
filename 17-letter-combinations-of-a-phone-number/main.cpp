006996f97a850122c7acf2704f707a8948eab920
Saul <saul@nixnux.org>
2021-06-06 22:39:08 -0700

    17-letter-combinations-of-a-phone-number started

---
+++ 17-letter-combinations-of-a-phone-number/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+std::string two = "abc";
+std::string three = "def";
+std::string four = "ghi";
+std::string five = "jkl";
+std::string six = "mno";
+std::string seven = "pqrs";
+std::string eight = "tuv";
+std::string nine = "wxyz";
+
+
+std::vector<std::string> letterCombinations(std::string digits) {
+
+    std::vector<std::string> s{""};
+    return s;
+}
+    
+int main() {
+    return 0;
+}
---
+++ 17-letter-combinations-of-a-phone-number/run.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+g++ -std=c++17 main.cpp -o main.o
+./main.o
